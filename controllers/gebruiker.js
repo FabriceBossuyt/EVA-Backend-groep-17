236079065a05f96a4829e1ab0e3fb4949f35b7c1
TomDeBacker <tom_10_10_10@hotmail.com>
2015-11-04 21:10:03 +0100

    Update gebruiker API

--- controllers/gebruiker.js
+++ controllers/gebruiker.js
@@ -56,14 +56,56 @@ exports.putGebruiker = function(req, res) {
       res.send(err);
 
     // Update the gebruiker attr
-    gebruiker.name = req.body.name;
+	if(req.body.username != null){
+		gebruiker.username = req.body.username;
+	}
+	if(req.body.hashedPassword != null){
+		gebruiker.hashedPassword = req.body.hashedPassword;
+	}
+	if(req.body.naam != null){
+		gebruiker.naam = req.body.naam;
+	}
+	if(req.body.voornaam != null){
+		gebruiker.voornaam = req.body.voornaam;
+	}
+	if(req.body.gedaneChallenges != null){
+		gebruiker.gedaneChallenges = req.body.gedaneChallenges;
+	}
+	if(req.body.facebookId != null){
+		gebruiker.facebookId = req.body.facebookId;
+	}
+	if(req.body.facebookToken != null){
+		gebruiker.facebookToken = req.body.facebookToken;
+	}
+	if(req.body.googleId != null){
+		gebruiker.googleId = req.body.googleId;
+	}
+	if(req.body.googleToken != null){
+		gebruiker.googleToken = req.body.googleToken;
+	}
+	if(req.body.salt != null){
+		gebruiker.salt = req.body.salt;
+	}
+	if(req.body.aantalDagen != null){
+		gebruiker.aantalDagen = req.body.aantalDagen;
+	}	
+	if(req.body.student != null){
+		gebruiker.student = req.body.student;
+	}	
+	if(req.body.geslacht != null){
+		gebruiker.geslacht = req.body.vegetarisch;
+	}	
+	if(req.body.vegetarisch != null){
+		gebruiker.vegetarisch = req.body.vegetarisch;
+	}
+    
 
     // Save the beer and check for errors
     gebruiker.save(function(err) {
       if (err)
         res.send(err);
 
-      res.json({message: 'Gebruiker saved', gebruiker});
+      res.json({message: 'Gebruiker updated and saved', gebruiker});
     });
   });
 };
